179506ff5690928101efccdac0d1f4f53bd7cf10
adiccsek <vadkerti-toth.adam@diak.szbi-pg.hu>
2024-10-09 10:40:00 +0200

    Simple Calculator Python Code

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def calculateSum(a, b):
+    return a + b
+def calculateDifference(a, b):
+    return a - b
+def calculateMultiply(a, b):
+    return a * b
+def calculateDivision(a, b):
+    return a / b
+
+
